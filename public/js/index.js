d70f4455cbbbaac5b738fad2268468d2de669d85
Jared Williams <jaredwilliamsmail@gmail.com>
2015-03-03 00:00:23 -0600

    added vids

--- public/js/index.js
+++ public/js/index.js
@@ -6,6 +6,19 @@ $('document').ready(function() {
   videoIdArray[3] = 'yzi3k7K0lFA';
   videoIdArray[4] = 'KDPTC-yAmgo';
   videoIdArray[5] = 'bK6hFk8jtvo';
+  videoIdArray[6] = 'ZJE7uJyHJ3k';
+  videoIdArray[7] = 'YXzubuENjHk';
+  videoIdArray[8] = '3O1rHYWH8MY';
+  videoIdArray[9] = 'heskWNMUqC8';
+  videoIdArray[10] = 'npjOSLCR2hE';
+  videoIdArray[11] = 'ijIq_-8HJo8';
+  videoIdArray[12] = 'cFW7yTOmRM8';
+  videoIdArray[13] = 'EdzqTGmEcZE';
+  videoIdArray[14] = 'f1oMhMwUbgc';
+  videoIdArray[15] = 'YUhb0XII93I';
+  videoIdArray[16] = 'akbflkF_1zY';
+  videoIdArray[17] = 'M_eYSuPKP3Y';
+  videoIdArray[18] = 'j1viagZK2-Q';
 
 	$('#wrapper').tubular({videoId: videoIdArray, start: 9, repeat: false});
 });
